fad7cb8b4ada37ea8ef9cd8d5ea1aa0179231d3e
AayushPandeyy <ap200061@gmail.com>
2020-08-03 22:31:58 +0545

    third commit

--- pro1.js
+++ pro1.js
@@ -2,6 +2,7 @@ shownotes()
 let addbtn=document.getElementById('addBtn')
 addbtn.addEventListener('click',function (e){
     let addTxt=document.getElementById('addTxt');
+    let addtitle=document.getElementById("addTitle");
     let notes=localStorage.getItem('notes');
     if (notes==null){
         notesObj=[]
@@ -9,7 +10,11 @@ addbtn.addEventListener('click',function (e){
     else{
         notesObj=JSON.parse(notes)
     }
-    notesObj.push(addTxt.value);
+    let myobj={
+        title:addtitle.value,
+        text:addTxt.value
+    }
+    notesObj.push(myobj);
     localStorage.setItem('notes',JSON.stringify(notesObj));
     addTxt.value=''//to remove the entered text in the text area.
     console.log(notesObj)
@@ -27,8 +32,8 @@ function shownotes(){
     notesObj.forEach(function (element,index){
         html+=`<div class="noteCard my-2 mx-2 card" style="width: 18rem;">
         <div class="card-body">
-            <h5 class="card-title">Note ${index + 1}</h5>
-            <p class="card-text"> ${element}</p>
+            <h5 class="card-title">${element.title}</h5>
+            <p class="card-text"> ${element.text}</p>
             <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
         </div>
     </div>`
